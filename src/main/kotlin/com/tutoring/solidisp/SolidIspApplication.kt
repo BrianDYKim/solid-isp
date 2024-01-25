bf0f20074886f9f717c5ba5ab91338c2d9e27dc1
BrianDYKim <18k7102dy@naver.com>
2024-01-25 20:23:46 +0900

    Initial commit

---
+++ src/main/kotlin/com/tutoring/solidisp/SolidIspApplication.kt
@@ -0,0 +1,11 @@
+package com.tutoring.solidisp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SolidIspApplication
+
+fun main(args: Array<String>) {
+    runApplication<SolidIspApplication>(*args)
+}
